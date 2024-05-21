2cf3b18e58ba6f0c9f196299763cd3616620f8db
Kuisin <kaisei0807s@gmail.com>
2024-05-21 10:54:30 +0900

    fix ios view v02

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, minimum-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover, minimum-scale=1, maximum-scale=1"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />

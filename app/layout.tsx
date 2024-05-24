f7566d95ca3e177ddf7ea96bd300f4d1d349b71c
Kuisin <kaisei0807s@gmail.com>
2024-05-24 18:27:50 +0900

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
 
         <meta
           name="viewport"

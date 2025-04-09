6b2760aa5eadbac081e9c037113e9c13a0fd7796
Kaisei Sawada <kaisei0807s@gmail.com>
2025-04-10 02:02:16 +0900

    remove name

--- app/page.tsx
+++ app/page.tsx
@@ -804,7 +804,7 @@ export default function Home() {
               </div>
             )}
             <div className="mt-3 mb-2 text-center text-xs">
-              Developed by Kaisei Sawada
+              Developed for GLOMAC
             </div>
           </div>
         </div>

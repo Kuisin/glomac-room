78620d086717059e0252d6d692f2f1a34a1ce9c3
Kaisei Sawada <kaisei0807s@gmail.com>
2025-04-10 17:53:33 +0900

    add auto map function to force page

--- app/admin/force/page.tsx
+++ app/admin/force/page.tsx
@@ -23,18 +23,20 @@ interface CsvData {
 }
 
 type HeaderMap = { [key: string]: string };
+type HeaderMapList = { [key: string]: string[] };
 
-const expectedHeaders: HeaderMap = {
-  title: "Title: 予約区分",
-  room: "Room: 施設コード(01FG-0-XXX)",
-  startTime: "Start Date & Time: 予約開始日時(yyyy/MM/dd HH:mm)",
-  endTime: "End Date & Time: 予約終了日時(yyyy/MM/dd HH:mm)",
+const expectedHeaders: HeaderMapList = {
+  title: ["Title: 予約区分", "予約区分"],
+  room: ["Room: 施設コード(01FG-0-XXX)", "施設コード"],
+  startTime: ["Start Date & Time: 予約開始日時(yyyy/MM/dd HH:mm)", "予約開始日時"],
+  endTime: ["End Date & Time: 予約終了日時(yyyy/MM/dd HH:mm)", "予約終了日時"],
   //   type: "Type",
-  user: "User",
-  description: "Description: 予約事由",
+  user: ["User", "予約者"],
+  description: ["Description: 予約事由", "予約事由"],
 };
 
 const mapHeaders = (row: CsvData, headerMap: HeaderMap): CsvData => {
+  console.log("headerMap", headerMap);
   const mappedRow: CsvData = {};
   for (const key in headerMap) {
     if (row[headerMap[key]] !== undefined) {
@@ -96,6 +98,18 @@ export default function BatchReservations() {
         complete: (result: any) => {
           const headers = result.meta.fields;
           setCsvHeaders(headers);
+
+          const autoMap: { [key: string]: string } = {};
+          headers.forEach((header: string) => {
+            // Check if header matches any expected header
+            Object.entries(expectedHeaders).forEach(([key, value]) => {
+              if (value[1] === header) {
+                autoMap[key] = header;
+              }
+            });
+          });
+
+          setHeaderMap(autoMap);
           setHeaderMapDialog(true);
         },
         error: (error: any) => {
@@ -411,6 +425,45 @@ export default function BatchReservations() {
                   </a>
                 </div>
               </div>
+
+              <div className="text-red-500 mb-2">{error}</div>
+              {isUploading ? (
+                <button
+                  type="button"
+                  className="flex items-center justify-center py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                >
+                  <div role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  onClick={handleUpload}
+                >
+                  Upload Data
+                  <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+                    {csvData.length}
+                  </span>
+                </button>
+              )}
               <div className="mb-4 bg-white shadow rounded-lg overflow-y-scroll">
                 <table className="min-w-full divide-y divide-gray-200 text-left">
                   <thead className="bg-gray-50">
@@ -507,44 +560,6 @@ export default function BatchReservations() {
                   </tbody>
                 </table>
               </div>
-              <div className="text-red-500 mb-2">{error}</div>
-              {isUploading ? (
-                <button
-                  type="button"
-                  className="flex items-center justify-center py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                >
-                  <div role="status">
-                    <svg
-                      aria-hidden="true"
-                      className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  onClick={handleUpload}
-                >
-                  Upload Data
-                  <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-                    {csvData.length}
-                  </span>
-                </button>
-              )}
             </div>
           </>
         )}
